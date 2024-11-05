e750d67c506d77053ac0d61b775f16b3d85a9027
Sushi <kaopansri@gmail.com>
2024-11-05 21:17:08 +0700

    feat(item): fix get item; add route and item.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import swagger from "@elysiajs/swagger";
 import { PrismaClient } from "@prisma/client";
 import { Elysia } from "elysia";
+import API from './module/item'
 
 const prisma = new PrismaClient();
 
@@ -8,28 +9,11 @@ const users = new Elysia({ prefix: "/users" })
   .get("/", () => "Users")
   .get("/profile", "Profile")
   .get("/settings", "Settings");
-
-const api = new Elysia({ prefix: "/api"})
-  .get("/", () => "API")
-  .get('/item', async() => {
-    const userid = BigInt(1)
-    const item_user = await prisma.user_item.findMany({
-      where: {
-        user_id: userid,
-      },
-      select: {
-        item_id: true,
-      }
-    })
-
-    const item_user_id = item_user.map(record => String(record.item_id))
-    return {"items": item_user_id}
-  })
     
 const app = new Elysia()
   .use(swagger())
   .use(users)
-  .use(api)
+  .use(API)
   .get("/", () => "Hello Elysia")
   .get("/test", "Testing")
   .listen(3000);
---
+++ src/module/item.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { Elysia } from "elysia";
+
+const prisma = new PrismaClient();
+
+const api = new Elysia({ prefix: "/api"})
+  .get("/", () => "API")
+  .get('/items', async() => {
+    const userid = BigInt(1)
+    const item_user = await prisma.user_item.findMany({
+      where: {
+        user_id: userid,
+      },
+      select: {
+        item_id: true,
+      }
+    })
+
+    const item_user_id = item_user.map(record => String(record.item_id))
+    return {"items": item_user_id}
+  })
+
+export default api;
