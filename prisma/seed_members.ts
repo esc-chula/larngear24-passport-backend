a021926e78085babf722f6bebfbb8fd2518c4344
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-12-26 22:15:25 +0700

    refactor: format

--- prisma/seed_members.ts
+++ prisma/seed_members.ts
@@ -1,9 +1,12 @@
-import fs from 'fs';
-import { PrismaClient} from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
 
 function csvToMembersData(csv: string) {
   const rows = csv.trim().split("\n");
-  const headers = rows.shift()?.split(",").map((h) => h.replace(/"/g, ""));
+  const headers = rows
+    .shift()
+    ?.split(",")
+    .map((h) => h.replace(/"/g, ""));
   if (!headers) return [];
 
   const membersData = rows.map((row) => {
@@ -14,7 +17,6 @@ function csvToMembersData(csv: string) {
       member[header] = values[index];
     });
 
-    
     return {
       lg_number: member["lg_number"],
       prefix: member["prefix"],
@@ -23,44 +25,56 @@ function csvToMembersData(csv: string) {
       nick_name: member["nick_name"],
       school: member["school"],
       baan: member["baan"],
-      google_id: member["googleId\r"].slice(0, member["googleId\r"].length -1),
+      google_id: member["google_id\r"].slice(
+        0,
+        member["google_id\r"].length - 1,
+      ),
     };
   });
 
   return membersData;
 }
 
-
 const mockDevMembersData = [
   {
-    "lg_number" : "LG_DEV001",
-    "prefix": "นางสาว",
-    "first_name": "ณภัทร",
-    "last_name": "เสรีรักษ์",
-    "nick_name": "นีร",
-    "school": "Chula",
-    "baan": "1",
-    "google_id": "117525997933765874261"
+    lg_number: "LG_DEV001",
+    prefix: "นางสาว",
+    first_name: "ณภัทร",
+    last_name: "เสรีรักษ์",
+    nick_name: "นีร",
+    school: "Chula",
+    baan: "1",
+    google_id: "117525997933765874261",
   },
   {
-    "lg_number" : "LG_DEV002",
-    "prefix": "นาย",
-    "first_name": "พี่โจ้",
-    "last_name": "jojo",
-    "nick_name": "jo",
-    "school": "Chula",
-    "baan": "1",
-    "google_id": "113926223118812005219"
+    lg_number: "LG_DEV002",
+    prefix: "นาย",
+    first_name: "พี่โจ้",
+    last_name: "jojo",
+    nick_name: "jo",
+    school: "Chula",
+    baan: "1",
+    google_id: "113926223118812005219",
+  },
+  {
+    lg_number: "LG_DEV003",
+    prefix: "นาย",
+    first_name: "Icy",
+    last_name: "Icy",
+    nick_name: "Icy",
+    school: "Chula",
+    baan: "1",
+    google_id: "111363166279026113290",
   },
 ];
 
 const prisma = new PrismaClient();
 async function main() {
   // gem Nong's data
-  const filePath = './prisma/bann_lg24_google_id.csv';
-  const csvInput = fs.readFileSync(filePath, 'utf8');
+  const filePath = "./prisma/bann_lg24_google_id.csv";
+  const csvInput = fs.readFileSync(filePath, "utf8");
   const mockMembersData = csvToMembersData(csvInput);
-  
+
   console.log("Start seeding...");
   const userMembers = await prisma.members.createMany({
     data: mockMembersData,
